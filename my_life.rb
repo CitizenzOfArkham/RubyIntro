0261d03397213379a65c72a1397f1e15b142d3b7
CitizenzOfArkham <danielrdzgmz@live.com>
2017-06-25 19:03:44 -0500

    Create my_life.rb

---
+++ my_life.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+#book reference
+
+my_life = "My life does not depend on the mistakes of my forefathers."
+
+puts my_life.upcase
+puts my_life.capitalize
+puts my_life.downcase
+puts my_life.reverse
+puts my_life.length
+
+
+$end
